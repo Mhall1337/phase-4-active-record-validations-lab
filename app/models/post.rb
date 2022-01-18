10825f1f3bcb8b95e1f46a665c8486663654ec7e
Mitchell <Hmitchell.hall@gmail.com>
2022-01-18 12:44:45 -0600

    commit tree 5adb3b7736b0a99281d2c5c9262384f306f9be8a

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,23 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: { in: ["Fiction", "Non-Fiction"]}
+    validate :click_bait_check
+
+    def fiction_or_not
+        if category == "Fiction"
+            true
+        elsif category == "Non-Fiction"
+            true
+        else
+            false
+        end
+    end
+    
+    def click_bait_check
+        if title == 'True Facts'
+            errors.add( :title, "sorry, this is clickbait")
+        end
+    end
 end
